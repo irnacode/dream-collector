cb3be800474b012b9f15022ad427a827012ccab7
Irnacode <irnamaarbun2404@gmail.com>
2026-06-27 07:20:50 +0700

    commit pertama saya

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { BrowserRouter, Routes, Route, Link, useParams } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, useParams, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { albums } from "./data/albums";
+import "./App.css";
+import "./index.css";
 
-    function Home() {
+   function Home() {
   return (
-    <div className="container">
+    <div className="hero-card">
       <h1>Dream Collector</h1>
 
       <p>
@@ -22,49 +24,46 @@ import { albums } from "./data/albums";
       </ul>
     </div>
   );
-}
+  }
 
 function Albums({ addToCollection }) {
   const [search, setSearch] = useState("");
 
   const filteredAlbums = albums.filter((album) =>
-  album.title.toLowerCase().includes(search.toLowerCase())
-);
+    album.title.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="container">
       <h1>NCT DREAM Albums</h1>
+
       <input
-  type="text"
-  placeholder="Search album..."
-  value={search}
-  onChange={(e) => setSearch(e.target.value)}
-/>
+        type="text"
+        placeholder="Search album..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
 
-      {filteredAlbums.map((album) => (
+      <div className="album-grid">
+        {filteredAlbums.map((album) => (
+          <div
+            key={album.id}
+            className="album-card"
+          >
+            <Link to={`/albums/${album.id}`}>
+              <h3>{album.title}</h3>
+            </Link>
 
-       <div
-  key={album.id}
-  style={{
-    backgroundColor: "white",
-    padding: "15px",
-    margin: "10px 0",
-    borderRadius: "10px",
-    boxShadow: "0 2px 5px rgba(0,0,0,0.1)"
-  }}
->
+            <p>{album.year}</p>
 
-         <Link to={`/albums/${album.id}`}>
-  <h3>{album.title}</h3>
-</Link>
-          <p>{album.year}</p>
-
-          <button onClick={() => addToCollection(album)}>
-  Add to Collection
-</button>
-          <hr />
-        </div>
-      ))}
+            <button
+              onClick={() => addToCollection(album)}
+            >
+              Add to Collection
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
@@ -87,6 +86,23 @@ function Collection({
   {collection.length} / {albums.length}
 </p>
       <p>{percentage}% Completed</p>
+      <div
+  style={{
+    width: "100%",
+    backgroundColor: "#ddd",
+    borderRadius: "10px",
+    height: "20px",
+  }}
+>
+  <div 
+    style={{
+      width: `${percentage}%`,
+      backgroundColor: "#00c73c",
+      height: "20px",
+      borderRadius: "10px",
+    }}
+  ></div>
+</div>
 
       {collection.length === 0 ? (
 
@@ -94,15 +110,9 @@ function Collection({
       ) : (
        collection.map((album) => (
   <div
-    key={album.id}
-    style={{
-      backgroundColor: "white",
-      padding: "15px",
-      margin: "10px 0",
-      borderRadius: "10px",
-      boxShadow: "0 2px 5px rgba(0,0,0,0.1)"
-    }}
-  >
+  key={album.id}
+  className="album-card"
+>
     <h3>{album.title}</h3>
     <p>{album.year}</p>
 
@@ -127,17 +137,21 @@ function AlbumDetail({ addToCollection }) {
     (album) => album.id === Number(id)
   );
 
+  if (!album) {
+    return <h2>Album not found</h2>;
+  }
+
   return (
-   <div className="container">
+    <div className="container">
       <h1>Album Detail</h1>
 
       <h2>{album.title}</h2>
-      
-      <p>Released: {album.year}</p> 
+
+      <p>Released: {album.year}</p>
 
       <button onClick={() => addToCollection(album)}>
-  Add to Collection
-</button>
+        Add to Collection
+      </button>
     </div>
   );
 }
@@ -161,6 +175,117 @@ function Wishlist() {
       <p>Albums I want to collect in the future.</p>
     </div>
   );
+}
+
+function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = () => {
+    const user = {
+      username,
+      password,
+    };
+
+    localStorage.setItem(
+      "user",
+      JSON.stringify(user)
+    );
+
+
+    alert("Register successful!");
+  };
+
+  return (
+    <div className="auth-card">
+      <h1>Register</h1>
+
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) =>
+          setUsername(e.target.value)
+        }
+      />
+
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) =>
+          setPassword(e.target.value)
+        }
+      />
+
+      <br /><br />
+
+      <button onClick={handleRegister}>
+        Register
+      </button>
+    </div>
+  );
+}
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    const savedUser = JSON.parse(
+      localStorage.getItem("user")
+    );
+
+    if (
+      savedUser &&
+      savedUser.username === username &&
+      savedUser.password === password
+    ) {
+      localStorage.setItem("isLoggedIn", "true");
+      window.location.href = "/";
+    } else {
+      alert("IWrong username or password");
+    }
+  };
+
+  return (
+    <div className="auth-card">
+  <h1>Login</h1>
+
+  <input
+    type="text"
+    placeholder="Username"
+    value={username}
+    onChange={(e) => setUsername(e.target.value)}
+  />
+
+  <br /><br />
+
+  <input
+    type="password"
+    placeholder="Password"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
+  />
+
+  <br /><br />
+
+  <button onClick={handleLogin}>
+    Login
+  </button>
+</div>
+  );
+}
+
+function ProtectedRoute({ children }) {
+  const isLoggedIn =
+    localStorage.getItem("isLoggedIn");
+
+  return isLoggedIn
+    ? children
+    : <Navigate to="/login" />;
 }
 
 function App() {
@@ -188,6 +313,11 @@ function App() {
     );
   };
 
+  const handleLogout = () => {
+  localStorage.removeItem("isLoggedIn");
+  window.location.href = "/login";
+};
+
   useEffect(() => {
     localStorage.setItem(
       "collection",
@@ -197,22 +327,37 @@ function App() {
 
   return (
     <BrowserRouter>
-      <nav>
-        <strong> Dream Collector</strong> |{" "}
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/albums">Albums</Link> |{" "}
-        <Link to="/collection">My Collection</Link> |{" "}
-        <Link to="/wishlist">Wishlist</Link> |{" "}
-        <Link to="/profile">Profile</Link>
-      </nav>
 
+      <nav className="navbar">
+  <div className="nav-left">
+    <strong>Dream Collector</strong>
+    <Link to="/">Home</Link>
+    <Link to="/albums">Albums</Link>
+    <Link to="/collection">Collection</Link>
+    <Link to="/wishlist">Wishlist</Link>
+    <Link to="/profile">Profile</Link>
+  </div>
+
+  <div className="nav-right">
+  {localStorage.getItem("isLoggedIn") ? (
+    <button onClick={handleLogout}>Logout</button>
+  ) : (
+    <>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+    </>
+  )}
+</div>
+</nav>
       <Routes>
         <Route path="/" element={<Home />} />
 
         <Route
           path="/albums"
           element={
-            <Albums addToCollection={addToCollection} />
+            <ProtectedRoute>
+              <Albums addToCollection={addToCollection} />
+            </ProtectedRoute>
           }
         />
 
@@ -242,6 +387,16 @@ function App() {
           path="/profile"
           element={<Profile />}
         />
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route
+          path="/register"
+          element={<Register />}
+        />
       </Routes>
 
       <footer
@@ -253,8 +408,9 @@ function App() {
       >
         Dream Collector © 2026
       </footer>
+
     </BrowserRouter>
   );
-}
+  }
 
 export default App;
